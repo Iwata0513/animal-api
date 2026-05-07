d684367817ccbcd93cd39a351e25ebc19adfe959
岩田 <masaki.iwata513@gmail.com>
2026-05-07 15:57:30 +0900

    一覧生成処理のリファクタリング

--- src/main/java/animal/service/AnimalInfoGetService.java
+++ src/main/java/animal/service/AnimalInfoGetService.java
@@ -14,23 +14,23 @@ import animal.data.AnimalSummary;
 public class AnimalInfoGetService {
 	
 	private final Map<String,Animal> animalMap = new HashMap<>();
-	private final List<AnimalSummary> animalSummaryList = new ArrayList<>();
 	
 	public AnimalInfoGetService() {
 		// 動物の情報全てを設定
 		animalMap.put("mikeneko", new Animal("三毛猫","猫","可愛い日本猫",15));
 		animalMap.put("shibainu", new Animal("芝犬","犬","もふもふな日本犬",15));
-		
-		// サマリ情報を生成
-		for(Map.Entry<String,Animal> entry : animalMap.entrySet()) {
-			AnimalSummary animal = new AnimalSummary(entry.getKey(), entry.getValue().getSpecies());
-			animalSummaryList.add(animal);
-		}
 	}
 	
 	// 登録中の動物一覧
 	public List<AnimalSummary>  getAnimalSummaries(){
-		return animalSummaryList;
+		
+		// サマリ情報を生成
+		return animalMap.entrySet().stream()
+		        .map(e -> new AnimalSummary(
+		                e.getKey(),
+		                e.getValue().getSpecies()
+		        ))
+		        .toList();
 	}
 	
 	// 登録中の動物情報全て
